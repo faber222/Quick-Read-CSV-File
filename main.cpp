45d85346defd93832ee0b551b745742b75f31956
faber222 <faberbernardo78@gmail.com>
2022-06-17 09:02:04 -0300

    New Features

--- main.cpp
+++ main.cpp
@@ -21,72 +21,73 @@ bool operator<(const index &d1, const index &d2) {
 
 arvore<index> indexacao;
 
+string separa(const string &texto, auto sep, string coluna, int &posicao) {
+  int x = 0;
+  string memoria;
+  while (x != string::npos) {
+    int z = texto.find_first_not_of(sep, x);
+    if (z == string::npos) break;
+    x = texto.find(sep, z);
+    memoria = texto.substr(z, x - z);
+    posicao++;
+    if (coluna == memoria) {
+      return memoria;
+      break;
+    }
+  }
+}
+
+string separa_coluna(const string &texto, auto sep, int &posicao) {
+  int x = 0;
+  string memoria;
+  int posicao_comparada = 0;
+  while (x != string::npos) {
+    int z = texto.find_first_not_of(sep, x);
+    if (z == string::npos) break;
+    x = texto.find(sep, z);
+    memoria = texto.substr(z, x - z);
+    posicao_comparada++;
+    if (posicao == posicao_comparada) {
+      return memoria;
+      break;
+    }
+  }
+}
+
 void indexar_dados(string nome_arq, string coluna_escolhida) {
   string leitura;
   string coluna;
-  string dado_index;
   string dado_coluna;
-  int pos1 = 0, pos2 = 0;
-  int ver_posicao = 1;
-  int posicao = 1;
+  string sep = ",";
+
+  int ver_posicao = 0;
   index referencia;
 
   ifstream arquivo(nome_arq);
+
   if (!arquivo.is_open()) {
     cout << "ARQUIVO INVALIDO OU INEXISTENTE" << endl;
     exit(true);
   }
 
   getline(arquivo, leitura);
-  while (true) {
-    pos1 = leitura.find_first_not_of(",", pos2);
-    pos2 = leitura.find_first_of(",", pos1 + 1);
-    if (pos1 == string::npos) {
-      break;
-    }
-    if (pos2 == string::npos) {
-      coluna = leitura.substr(pos1);
-      break;
-    }
-    coluna = leitura.substr(pos1, pos2 - pos1);
-    if (coluna == coluna_escolhida) {
-      break;
-    }
-    ver_posicao++;
-  }
+
+  coluna = separa(leitura, sep, coluna_escolhida, ver_posicao);
 
   if (coluna != coluna_escolhida) {
-    cout << coluna << endl;
     cout << "A COLUNA NAO EXISTE" << endl;
     exit(true);
   }
 
   while (getline(arquivo, leitura)) {
-    pos1 = 0;
-    pos2 = 0;
-    posicao = 1;
-    while (true) {
-      pos1 = leitura.find_first_not_of(",", pos2);
-      pos2 = leitura.find_first_of(",", pos1 + 1);
-      if (pos1 == string::npos) {
-        break;
-      }
-      if (pos2 == string::npos) {
-        dado_coluna = leitura.substr(pos1);
-        referencia.coluna = dado_coluna;
-        referencia.pos_linha = arquivo.tellg();
-        indexacao.adiciona(referencia);
-        break;
-      }
-      dado_coluna = leitura.substr(pos1, pos2 - pos1);
-      if (posicao == ver_posicao) {
-        referencia.coluna = dado_coluna;
-        referencia.pos_linha = arquivo.tellg();
-        indexacao.adiciona(referencia);
-        break;
-      }
-      posicao++;
-    }
+    int posicao = 1;
+    dado_coluna = separa_coluna(leitura, sep, ver_posicao);
+    referencia.coluna = dado_coluna;
+    referencia.pos_linha = arquivo.tellg();
+    indexacao.adiciona(referencia);
+  }
+  for (auto &x : indexacao) {
+    cout << x.coluna << " " << x.pos_linha << endl;
   }
 }
 
